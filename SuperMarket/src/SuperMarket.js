4b33756e7ab2fc13a131d24d611638d5f64d9687
Rich <richhildebrand@gmail.com>
2013-07-03 19:50:37 -0400

    Green - People buy more bread than soup.

--- SuperMarket/src/SuperMarket.js
+++ SuperMarket/src/SuperMarket.js
@@ -3,7 +3,9 @@ function() {
 	'use strict';
 
 	var SuperMarket = function(loafsOfBread) {
+		if (loafsOfBread < 5) { return '$1' }
 		return "$" + loafsOfBread.toString();
+
 	};
 
 	return SuperMarket;
