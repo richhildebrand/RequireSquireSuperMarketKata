bd4c5b3719b2709fca9e708b95dce1943c0801d6
Eric Fleming <eric-fleming18@hotmail.com>
2013-07-04 12:31:50 -0400

    Green - Now adds a zero to the end of the decimal

--- SuperMarket/src/SuperMarket.js
+++ SuperMarket/src/SuperMarket.js
@@ -4,12 +4,19 @@ function() {
 
 	var SuperMarket = function(itemsToBuy) {
 		var price = '$';
+		var zero = '0';
+		var complete = '';
 
 		if (itemsToBuy['loafsOfBread'])
 		{
 			return price + itemsToBuy['loafsOfBread'] 
 		}
-		return price + itemsToBuy['noodles'] * .5;
+		complete = price + itemsToBuy['noodles'] * .5;
+		if (complete.toString().indexOf(".") != -1)
+		{
+			return complete + zero;
+		}
+		return complete;
 	};
 
 	return SuperMarket;
