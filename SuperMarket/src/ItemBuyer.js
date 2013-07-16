d088db57f5b3972fddd3fcd7cfdd57c73e7a3bc8
Rich <richhildebrand@gmail.com>
2013-07-15 21:02:35 -0400

    Red - Unit it tests should now hit buyItem

--- SuperMarket/src/ItemBuyer.js
+++ SuperMarket/src/ItemBuyer.js
@@ -2,11 +2,8 @@ define(['PriceFormatter', 'ProductList'],
 function(PriceFormatter, ProductList) {
 	'use strict';
 
-	var _priceFormatter = PriceFormatter;
-
 	var ItemBuyer = {
 			
-		//TODO: do not pass in orderResult
 		buyItems: function(itemsToBuy) {
 			var me = this;
 			var orderResult = { totalPrice: 0, receipt: ""};
@@ -15,20 +12,8 @@ function(PriceFormatter, ProductList) {
 			   orderResult = me.buyItem(itemsToBuy, orderResult, key);
 			});
 
-			orderResult.totalPrice = (_priceFormatter.formatPrice(orderResult.totalPrice));
+			orderResult.totalPrice = (PriceFormatter.formatPrice(orderResult.totalPrice));
 			return orderResult;
-		},
-
-		buyBread: function(itemsToBuy, orderResult) {
-			return this.buyItem(itemsToBuy, orderResult, 'loafsOfBread');
-		},
-
-		buyNoodles: function(itemsToBuy, orderResult) {
-			return this.buyItem(itemsToBuy, orderResult, 'noodles');
-		},
-
-		buySoup: function(itemsToBuy, orderResult) {
-			return this.buyItem(itemsToBuy, orderResult, 'cansOfSoup');
 		},
 
 		buyItem: function(itemsToBuy, orderResult, itemToBuy) {
