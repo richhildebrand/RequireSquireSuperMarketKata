06d5145347df575d774e966d0611b1304652c16a
Eric Fleming <eric-fleming18@hotmail.com>
2013-07-15 22:35:16 -0400

    Red - I want my receipt to be correct

---
+++ SuperMarket/spec/unitTests/buyItems.js
@@ -0,0 +1,39 @@
+define(['Squire'], function(Squire) {
+	describe('Unit Test - buyItems function', function() {
+	'use strict';
+
+		var testContext = {};
+		var _itemBuyer;
+		var _order;
+		var _orderResult
+
+		beforeEach(function(done) {
+			_order = {};
+			_orderResult = { totalPrice: 0, receipt: "" };
+
+			testContext.injector = new Squire();
+
+			testContext.injector.require(['ItemBuyer'], function(ItemBuyer) {
+				testContext.ItemBuyer = ItemBuyer;
+				_itemBuyer = testContext.ItemBuyer;
+				done();
+			});
+		});
+
+		describe('When bread costs 1 per loaf, noodles cost .5, and soup costs 2 per can, buyItems', function() {
+			it('should charge me 5 for 2 loafs of bread, 2 noodles, and 1 can of soup', function() {
+				_order['loafsOfBread'] = 2;
+				_order['noodles'] = 2;
+				_order['cansOfSoup'] = 1;
+				
+				var orderResult = _itemBuyer.buyItems(_order);
+				var costOfOrder = orderResult.totalPrice;
+				var receipt = orderResult.receipt;
+
+				expect(costOfOrder).to.be.equal('$5');
+				expect(receipt).to.be.equal('loafsOfBread: $2, noodles: $1, cansOfSoup: $2');
+			});
+		});
+
+	});
+});
