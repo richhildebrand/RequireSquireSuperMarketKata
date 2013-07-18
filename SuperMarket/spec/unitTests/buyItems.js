a92276a3d71c79ffa4920fe2b1ec148426505931
Eric Fleming <eric-fleming18@hotmail.com>
2013-07-18 09:36:57 -0400

    Red - Add in a mocked product list for buyItem. And See comments

--- SuperMarket/spec/unitTests/buyItems.js
+++ SuperMarket/spec/unitTests/buyItems.js
@@ -1,4 +1,4 @@
-define(['Squire'], function(Squire) {
+define(['Squire', 'ProductList'], function(Squire, ProductList) {
 	describe('Unit Test - buyItems function', function() {
 	'use strict';
 
@@ -12,6 +12,7 @@ define(['Squire'], function(Squire) {
 			_orderResult = { totalPrice: 0, receipt: "" };
 
 			testContext.injector = new Squire();
+			testContext.injector.mock(ProductList, 'ProductList');
 
 			testContext.injector.require(['ItemBuyer'], function(ItemBuyer) {
 				testContext.ItemBuyer = ItemBuyer;
@@ -21,20 +22,29 @@ define(['Squire'], function(Squire) {
 		});
 
 
-		describe('When bread costs 1 per loaf, noodles cost .5, and soup costs 2 per can, buyItems', function() {
-			it('should charge me 5 for 2 loafs of bread, 2 noodles, and 1 can of soup', function() {
+		describe('When bread costs 2 per loaf, noodles cost 1, and soup costs 4 per can, buyItems', function() {
+			it('should charge me 10 for 2 loafs of bread, 2 noodles, and 4 can of soup', function() {
 				_order['loafsOfBread'] = 2;
 				_order['noodles'] = 2;
 				_order['cansOfSoup'] = 1;
+
+				//I did this simple red so you have a chance to do another mock in the agreed upon way.
+				//Just make the mock use the correct values basically. Don't forget the after each! :)
+
+				//Also, I think we need to write some more acceptance tests. Another good use I'm finding with acceptance tests
+				//is that for a long project like this it would be useful to have tests in place for plently for
+				//future functionality, even if the individual test structure completely changes when we get around to it
+				//, it would still be nice to have the direction and idea we originally had.
+				//Thoughts?
 				
 				var orderResult = _itemBuyer.buyItems(_order);
 				var costOfOrder = orderResult.totalPrice;
 				var receipt = orderResult.receipt;
 
-				expect(costOfOrder).to.be.equal('$5');
-				expect(receipt).to.be.contain('loafsOfBread: $2');
-				expect(receipt).to.be.contain('noodles: $1');
-				expect(receipt).to.be.contain('cansOfSoup: $2');
+				expect(costOfOrder).to.be.equal('$10');
+				expect(receipt).to.be.contain('loafsOfBread: $4');
+				expect(receipt).to.be.contain('noodles: $2');
+				expect(receipt).to.be.contain('cansOfSoup: $4');
 			});
 		});
 
